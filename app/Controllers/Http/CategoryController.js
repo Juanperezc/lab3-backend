e89ed4815867d5f5ce0dcd5c2a131898c5588605
leon <leonluis_23@yahoo.es>
2016-02-11 16:50:07 -0430

    modificado catgeroyController

--- app/Controllers/Http/CategoryController.js
+++ app/Controllers/Http/CategoryController.js
@@ -2,27 +2,29 @@
 const Categoria = use('App/Models/Category');
 
 class CategoryController {
-   //Listar Categorias
+   //Listar Categorias -> /categorys 
     async index({ request, response }) {
         const cat = await Categoria.all();
         return response.json({"categorias": cat})
     }
-    //buscar categoria x id
+    //buscar categoria x id-> /categorys/:id 
     async show({params, request, response}){
         const id = params.id;
         const cat = await Categoria.find(id)
 
         return response.json({"categoria": cat});
     }
-    //Crear una categoria
+    //Crear una categoria -> /categorys
     async store({request, response}){
-        const cat = request.body;
-        if(cat.name.length  && cat.status)
-            Categoria.create(cat)
+        const data = request.body;
+        let cat = null;
+
+        if(data.name.length  && data.status)
+          cat =  await Categoria.create(data)
 
         return response.json({"category": cat})
     }
-    //Eliminar categoria x id
+    //Eliminar categoria x id -> /categorys/:id 
     async destroy({params,request,response}){
         const id = params.id;
         const cat = await Categoria.find(id);
@@ -31,15 +33,14 @@ class CategoryController {
 
         return response.json({"category": cat})
     }
-    //editar una categoria
+    
+    //editar una categoria -> /categorys/:id 
     async update({params,request,response}){
         const id = params.id;
         const cat = await Categoria.find(id);
 
         const name = request.body.name;
-
         cat.name = name;
-
         cat.save();
 
         return response.json({"category": cat})
