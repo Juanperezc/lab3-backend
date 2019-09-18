61ff4e7303ee95c1c06a6b38720ef4457eb47e2f
Juan Perez <juanl1996@hotmail.com>
2019-09-18 17:57:55 -0400

    cambios user

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -5,13 +5,8 @@ const Schema = use('Schema')
 
 class UserSchema extends Schema {
   up () {
-    this.create('users', (table) => {
-      table.increments()
-      table.string('username', 80).notNullable().unique()
-      table.string('email', 254).notNullable().unique()
-      table.string('password', 60).notNullable()
-      table.string('name', 60).notNullable()
-      table.timestamps()
+    this.collection('users', (collection) => {
+      collection.index('email_index', {email: 1}, {unique: true})
     })
   }
 
