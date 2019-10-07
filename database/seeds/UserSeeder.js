f17c661669a331df0fa3ae7076381c9dd86a6344
Marco Saenz <37160343+SullenestDust1@users.noreply.github.com>
2019-10-06 21:21:30 -0400

    Otro seeder

--- database/seeds/UserSeeder.js
+++ database/seeds/UserSeeder.js
@@ -16,45 +16,86 @@ const moment = require('moment')
 const User = use('App/Models/User')
 
 class UserSeeder {
-  static async run () {
-    await User.create({ email: 'juanl1996@hotmail.com',
-    full_name :  'Juan Perez',
-    date_birth : '',
-    phone:  '04245869872',
-    profile_pic : 'test',
-    password: '2514182657',
-    city : 'Barquisimeto',
-    alias: 'juanperezc',
-    rol: 'admin',
-    threads : [
-      {
-        user_alias: 'marcosaenz',
-        messages:[{
-          body: 'Hola',
-          sender_alias: 'juanperezc',
-          created_at : moment().toDate(),
+  static async run() {
+    await User.create({
+      email: 'juanl1996@hotmail.com',
+      full_name: 'Juan Perez',
+      date_birth: '',
+      phone: '04245869872',
+      profile_pic: 'test',
+      password: '2514182657',
+      city: 'Barquisimeto',
+      alias: 'juanperezc',
+      rol: 'admin',
+      threads: [
+        {
+          user_alias: 'marcosaenz',
+          messages: [{
+            body: 'Hola',
+            sender_alias: 'juanperezc',
+            created_at: moment().toDate(),
+            updated_at: moment().toDate(),
+          }],
+          created_at: moment().toDate(),
           updated_at: moment().toDate(),
-        }],
-        created_at : moment().toDate(),
+        }
+      ],
+      notifications: [
+        {
+          type: 'Request',
+          content: 'data',
+          read_at: null,
+          created_at: moment().toDate(),
+          updated_at: moment().toDate(),
+        }
+      ],
+      followers: [{
+        user_alias: 'marcosaenz',
+        created_at: moment().toDate(),
         updated_at: moment().toDate(),
       }
-    ],
-    notifications:[
-      {
-        type: 'Request',
-        content: 'data',
-        read_at: null,
-        created_at : moment().toDate(),
+      ]
+    });
+    await User.create({
+      email: 'test@test.com'
+      ,
+      full_name: 'Marco Saenz',
+      date_birth: '',
+      phone: '04245774672',
+      profile_pic: 'test1',
+      password: '1234567890',
+      city: 'Cabudare',
+      alias: 'marcosaenz',
+      rol: 'admin',
+      threads: [
+        {
+          user_alias: 'juanperezc',
+          messages: [{
+            body: 'Hola',
+            sender_alias: 'marcosaenz',
+            created_at: moment().toDate(),
+            updated_at: moment().toDate(),
+          }],
+          created_at: moment().toDate(),
+          updated_at: moment().toDate(),
+        }
+      ],
+      notifications: [
+        {
+          type: 'Request',
+          content: 'data',
+          read_at: null,
+          created_at: moment().toDate(),
+          updated_at: moment().toDate(),
+        }
+      ],
+      followers: [{
+        user_alias: 'juanperezc',
+        created_at: moment().toDate(),
         updated_at: moment().toDate(),
       }
-    ],
-    followers:[{
-      user_alias: 'marcosaenz',
-      created_at : moment().toDate(),
-      updated_at: moment().toDate(),
-    }
-    ]});
-   // await User.create({ email: 'juanlxboxes@gmail.com' });
+      ]
+    });
   }
 }
 
