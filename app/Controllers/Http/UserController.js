bfa334429c69feb44974fa229411e3fede3cfac4
Juan Perez <juanl1996@hotmail.com>
2019-11-03 12:23:10 -0400

    cambios usercontroller

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -17,7 +17,7 @@ class UserController {
             const user_model = await User.find(user._id).with('publications')
             return response.json({"user": user_model})
           } catch (error) {
-            response.status(401).json('Missing or invalid jwt')
+            response.status(500).json('error', error);
           }
 
     }
